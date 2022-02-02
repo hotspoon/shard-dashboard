abb2ee2e761f36cacfbb518a440fdff9b9c1f03a
faris18 <fariz@gdc.co.id>
2022-02-02 17:22:15 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "datatables.net-dt/js/dataTables.dataTables";
+import "./shards-dashboard/styles/shards-dashboards.1.3.1.min.css";
+
+import Tableku from "./components/Tableku";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="my-5">
+      <div className="container">
+        <Tableku />
+      </div>
     </div>
   );
 }
