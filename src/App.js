8e9dbb7096c9c6b6d75921bb238ca4cd7d741ee7
faris18 <fariz@gdc.co.id>
2022-02-03 11:43:44 +0700

    initial commit v3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import routes from "./routes";
+import withTracker from "./withTracker";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "datatables.net-dt/js/dataTables.dataTables";
 import "./shards-dashboard/styles/shards-dashboards.1.3.1.min.css";
 
-import Tableku from "./components/Tableku";
-
-function App() {
-  return (
-    <div className="my-5">
-      <div className="container">
-        <Tableku />
-      </div>
+export default () => (
+  <Router basename={process.env.REACT_APP_BASENAME || ""}>
+    <div>
+      {routes.map((route, index) => {
+        return (
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            component={withTracker(props => {
+              return (
+                <route.layout {...props}>
+                  <route.component {...props} />
+                </route.layout>
+              );
+            })}
+          />
+        );
+      })}
     </div>
-  );
-}
+  </Router>
+);
 
-export default App;
